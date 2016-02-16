1d62845137cc71be2b0876c0b51cfa305ed47c2e
Alvin Meimoun <alvin.meimoun@gmail.com>
2016-02-16 13:27:36 +0100

    feat(auth): Implemented security filters

---
+++ 4JVA_SupFitness/4JVA_SupFitness-war/src/java/com/supinfo/supfitness/web/filter/AuthCheckerFilter.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supinfo.supfitness.web.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class AuthCheckerFilter implements Filter {
+
+    public AuthCheckerFilter() {
+    }
+
+    public void destroy() {
+        
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        List<Cookie> cookies;
+        if(req.getCookies() != null){
+            cookies = Arrays.asList(req.getCookies());
+        } else {
+            cookies = new ArrayList<>();
+        }
+        
+        boolean validCredentials = false;
+        String username = "";
+        String token = "";
+        
+        for(Cookie c : cookies){
+            if("sb_username".equals(c.getName())){
+                username = c.getValue();
+            }
+            if("sb_token".equals(c.getName())){
+                token = c.getValue();
+            }
+        }
+        
+        if(username != null && username.length() > 0 
+                && DigestUtils.sha256Hex(username).equals(token)){
+            validCredentials = true;
+        }
+        
+        
+        if (validCredentials) {
+            chain.doFilter(request, response);
+        } else {
+            ((HttpServletResponse)response).sendRedirect("home");
+        }
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+       
+    }
+
+
+}
---
+++ 4JVA_SupFitness/4JVA_SupFitness-war/src/java/com/supinfo/supfitness/web/filter/AuthManagerFilter.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.supinfo.supfitness.web.filter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class AuthManagerFilter  implements Filter {
+
+    public AuthManagerFilter() {
+    }
+
+    public void destroy() {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        // TODO Auto-generated method stub
+        // place your code here
+        HttpServletRequest req = (HttpServletRequest) request;
+        List<Cookie> cookies;
+        if(req.getCookies() != null){
+            cookies = Arrays.asList(req.getCookies());
+        } else {
+            cookies = new ArrayList<>();
+        }
+        
+        boolean validCredentials = false;
+        String username = "";
+        String token = "";
+        
+        for(Cookie c : cookies){
+            if("sb_username".equals(c.getName())){
+                username = c.getValue();
+            }
+            if("sb_token".equals(c.getName())){
+                token = c.getValue();
+            }
+        }
+        
+        if(username != null && username.length() > 0 
+                && DigestUtils.sha256Hex(username).equals(token)){
+            validCredentials = true;
+        } else {
+            username = "";
+        }
+        
+        
+        ((HttpServletRequest)request).setAttribute("isAuthenticated", 
+                String.valueOf(validCredentials));
+        ((HttpServletRequest)request).setAttribute("username", username);
+        
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+       
+    }
+
+
+}
